f7d0a93ad6d5a92f4f4c5ad68afcc5e62453e7ad
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-16 16:56:36 -0600

    front end create functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,24 @@ class ItemsController < ApplicationController
     render json: @items
   end 
 
+  def create 
+    @item = Item.new(items_params)
+    if @item.save
+      render json: @item
+    else 
+      render json: {error:422, message: @item.errors.full_messages}
+    end 
+  end 
+
   def destroy
     @item = Item.find(params[:id]).destroy
     render json: @item
+  end 
+
+  private
+
+  def items_params 
+    params.require(:item).permit(:name, :description, :category, :likes)
   end 
 
 end
